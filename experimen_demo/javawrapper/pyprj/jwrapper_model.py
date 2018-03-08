2c706d7f83dac857c7723e70b2fb5f39bb057346
butter <123@123.com>
2018-03-08 08:32:39 +0800

    test env

--- experimen_demo/javawrapper/pyprj/jwrapper_model.py
+++ experimen_demo/javawrapper/pyprj/jwrapper_model.py
@@ -32,7 +32,7 @@ import sys
 
 import tensorflow as tf
 
-import mnist_input_data
+# import mnist_input_data
 
 tf.app.flags.DEFINE_integer('training_iteration', 1000,
                             'number of training iterations.')
@@ -42,54 +42,53 @@ FLAGS = tf.app.flags.FLAGS
 
 
 def main(_):
-  if len(sys.argv) < 2 or sys.argv[-1].startswith('-'):
-    print('Usage: mnist_export.py [--training_iteration=x] '
-          '[--model_version=y] export_dir')
-    sys.exit(-1)
-  if FLAGS.training_iteration <= 0:
-    print 'Please specify a positive value for training iteration.'
-    sys.exit(-1)
-  if FLAGS.model_version <= 0:
-    print 'Please specify a positive value for version number.'
-    sys.exit(-1)
+  # if len(sys.argv) < 2 or sys.argv[-1].startswith('-'):
+  #   print('Usage: mnist_export.py [--training_iteration=x] '
+  #         '[--model_version=y] export_dir')
+  #   sys.exit(-1)
+  # if FLAGS.training_iteration <= 0:
+  #   print 'Please specify a positive value for training iteration.'
+  #   sys.exit(-1)
+  # if FLAGS.model_version <= 0:
+  #   print 'Please specify a positive value for version number.'
+  #   sys.exit(-1)
 
   # Train model
   print 'Training model...'
-  mnist = mnist_input_data.read_data_sets(FLAGS.work_dir, one_hot=True)
+  # mnist = mnist_input_data.read_data_sets(FLAGS.work_dir, one_hot=True)
   sess = tf.InteractiveSession()
   serialized_tf_example = tf.placeholder(tf.string, name='tf_example')
-  feature_configs = {'x': tf.FixedLenFeature(shape=[784], dtype=tf.float32),}
+  feature_configs = {'x': tf.FixedLenFeature(shape=[5], dtype=tf.float32),}
   tf_example = tf.parse_example(serialized_tf_example, feature_configs)
   x = tf.identity(tf_example['x'], name='x')  # use tf.identity() to assign name
-  y_ = tf.placeholder('float', shape=[None, 10])
-  w = tf.Variable(tf.zeros([784, 10]))
-  b = tf.Variable(tf.zeros([10]))
+  y_ = tf.placeholder('float', shape=[None, 2])
+  w = tf.Variable(tf.zeros([5, 2]))
+  b = tf.Variable(tf.zeros([2]))
   sess.run(tf.global_variables_initializer())
   y = tf.nn.softmax(tf.matmul(x, w) + b, name='y')
   cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
   train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-  values, indices = tf.nn.top_k(y, 10)
+  values, indices = tf.nn.top_k(y, 2)
   table = tf.contrib.lookup.index_to_string_table_from_tensor(
-      tf.constant([str(i) for i in xrange(10)]))
+      tf.constant([str(i) for i in range(2)]))
   prediction_classes = table.lookup(tf.to_int64(indices))
   for _ in range(FLAGS.training_iteration):
-    batch = mnist.train.next_batch(50)
-    train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+    # batch = mnist.train.next_batch(50)
+    train_step.run(feed_dict={x: [[1.0,2.0,3.0,4.0,5.0]], y_: [[1.0,0.0]]})
   correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-  accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
-  print 'training accuracy %g' % sess.run(
-      accuracy, feed_dict={x: mnist.test.images,
-                           y_: mnist.test.labels})
+  # accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
+  # print 'training accuracy %g' % sess.run(
+  #     accuracy, feed_dict={x: mnist.test.images,
+  #                          y_: mnist.test.labels})
   print 'Done training!'
-
   # Export model
   # WARNING(break-tutorial-inline-code): The following code snippet is
   # in-lined in tutorials, please update tutorial documents accordingly
   # whenever code changes.
-  export_path_base = sys.argv[-1]
+  export_path_base = "/tmp/jwrapper_mdl" #sys.argv[-1]
   export_path = os.path.join(
       tf.compat.as_bytes(export_path_base),
-      tf.compat.as_bytes(str(FLAGS.model_version)))
+      tf.compat.as_bytes(str(1))) #tf.compat.as_bytes(str(FLAGS.model_version)))
   print 'Exporting trained model to', export_path
   builder = tf.saved_model.builder.SavedModelBuilder(export_path)
 
